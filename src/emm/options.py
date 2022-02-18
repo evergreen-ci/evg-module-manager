d85dd7bd1211ebf95dbecbf119a7b77495da9c54
David Bradford <david.bradford@mongodb.com>
2022-02-18 16:26:45 -0500

    Add ability to configure via local file

--- src/emm/options.py
+++ src/emm/options.py
@@ -1,7 +1,13 @@
 """Configuration options for evg-module-manager."""
+from __future__ import annotations
+
 import os
 from dataclasses import dataclass
 from pathlib import Path
+from typing import Optional
+
+import yaml
+from pydantic import BaseModel
 
 DEFAULT_MODULES_PATH = ".."
 DEFAULT_EVG_CONFIG = os.path.expanduser("~/.evergreen.yml")
@@ -22,3 +28,35 @@ class EmmOptions:
     modules_directory: Path = Path(DEFAULT_MODULES_PATH)
     evg_config: Path = Path(DEFAULT_EVG_CONFIG)
     evg_project: str = DEFAULT_EVG_PROJECT
+
+
+class EmmConfiguration(BaseModel):
+    """
+    Model for saving the EMM configuration.
+
+    * evg_project: Evergreen project being executed against.
+    * modules_directory: Directory to store repositories of modules.
+    """
+
+    evg_project: Optional[str]
+    modules_directory: Optional[str]
+
+    @classmethod
+    def from_yaml_file(cls, filename: Path) -> EmmConfiguration:
+        """
+        Read the emm configuration from a yaml files.
+
+        :param filename: File to read configuration from.
+        :return: Model read from given file.
+        """
+        with open(filename) as f:
+            return cls(**yaml.safe_load(f))
+
+    def save_yaml_file(self, filename: Path) -> None:
+        """
+        Write the emm configuration to a yaml files.
+
+        :param filename: File to write configuration to.
+        """
+        with open(filename, "w") as f:
+            f.write(yaml.safe_dump(self.dict(exclude_none=True, exclude_unset=True)))
